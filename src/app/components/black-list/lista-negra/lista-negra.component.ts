52506a0c77be68089bce97e54efbfd2dd5c4daf8
WallaceGaster <saitama.kurosakyy@gmail.com>
2026-03-26 14:58:10 -0600

    actualizando lista negra

--- src/app/components/black-list/lista-negra/lista-negra.component.ts
+++ src/app/components/black-list/lista-negra/lista-negra.component.ts
@@ -1,12 +1,6 @@
-import { Component, Input, Output, EventEmitter,OnInit  } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { PacientesService } from 'src/app/services/pacientes.service';
-import Swal from 'sweetalert2';
-import { LoginService } from 'src/app/services/login.service';
-import { ModalComponent } from '../../modal/modal.component';
-import { FormsModule } from '@angular/forms';
-import { ListaNegraService } from 'src/app/services/lista-negra.service';
 @Component({
   selector: 'app-lista-negra',
   standalone: true,
@@ -14,34 +8,24 @@ import { ListaNegraService } from 'src/app/services/lista-negra.service';
   templateUrl: './lista-negra.component.html',
   styleUrls: ['./lista-negra.component.scss']
 })
-export class ListaNegraComponent implements OnInit {
-  pacientesListaNegra: any[] = [];
-  @Input()entrada: any;
-  paciente: any;
+export class ListaNegraComponent {
+  @Input() entrada: any;
   showDetails = false;
 
+  constructor(private router: Router) {}
 
-  constructor(
-    private router: Router,
-    private pacientesService: PacientesService,
-    private listaNegraService: ListaNegraService) {}
-
-  ngOnInit(): void {
-    console.log('Datos recibidos en entrada:', this.entrada);
-    this.listaNegraService.getPacientesListaNegra().subscribe({
-      next: (res) => {
-        this.pacientesListaNegra = res.data;
-      },
-      error: (err) => {
-        console.error('Error al obtener la lista negra:', err);
-      }
-    });
-  }
   verPerfil(idPaciente: string) {
-    this.router.navigate(['/perfil', 'paciente', idPaciente]);
+    if (idPaciente) {
+      this.router.navigate(['/perfil', 'paciente', idPaciente]);
+    }
+  }
+  
+
+  toggleDetails(): void {
+    this.showDetails = !this.showDetails;
   }
 
-  formatearFecha(fechaNacimiento: string): string {
+formatearFecha(fechaNacimiento: string): string {
     if (!fechaNacimiento) return 'No proporcionada';
 
     // Usar fecha en formato ISO (YYYY-MM-DD) sin ajustar por zona horaria
@@ -51,11 +35,5 @@ export class ListaNegraComponent implements OnInit {
 
     return `${mes.padStart(2, '0')}/${dia.padStart(2, '0')}/${año}`;
   }
-
-   toggleDetails(): void {
-    this.showDetails = !this.showDetails;
-  }
-
-  
 
 }
